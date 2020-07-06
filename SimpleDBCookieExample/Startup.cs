20d6f888c7d9edb59bcc494a30c9f35112a5aaa8
Andrew Douglas <potterboy67@outlook.com>
2020-07-06 13:20:24 +0100

    Added a simple controller and home view index, and some scaffold stuff.

--- SimpleDBCookieExample/Startup.cs
+++ SimpleDBCookieExample/Startup.cs
@@ -16,6 +16,7 @@ namespace SimpleDBCookieExample
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +27,22 @@ namespace SimpleDBCookieExample
                 app.UseDeveloperExceptionPage();
             }
 
+            // An intercept, if we need it, in the middleware stack.
+            app.Use(async (context, next) =>
+            {                
+                // Do work that doesn't write to the Response.
+                await next.Invoke();
+                // Do logging or other work that doesn't write to the Response.
+            });
+
             app.UseRouting();
 
+            app.UseCookiePolicy();
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
